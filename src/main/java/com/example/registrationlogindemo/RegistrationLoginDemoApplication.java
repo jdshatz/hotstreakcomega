1f2a508946c6b95ac3fed4b9c10476ec410796cf
jdshatz <jshatz@optonline.net>
2023-10-07 09:48:03 -0400

    servlet fix

--- src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
+++ src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -2,10 +2,16 @@ package com.example.registrationlogindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RegistrationLoginDemoApplication {
+public class RegistrationLoginDemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistrationLoginDemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
 	}
