35479b17001fb0d58073590135fb3724f72e7f73
jdshatz <jshatz@optonline.net>
2023-10-08 23:12:51 -0400

    override

--- src/main/java/com/example/registrationlogindemo/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.registrationlogindemo.service;
-
-import com.example.registrationlogindemo.model.Mail;
-
-public interface EmailService {
-        public void sendEmail(Mail mail);
-    }
